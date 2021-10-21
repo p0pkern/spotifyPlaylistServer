92428d9128c459b98313b1e9f474758bef9321b2
p0pkern <petermanchristoph@gmail.com>
2021-10-21 16:11:12 -0400

    Added credential refresh, and spotify data return

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 require('dotenv').config();
 const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const util = require('util');
+const router = express.Router();
+const path = require('path');
+const bodyParser = require('body-parser')
 
 const app = express();
-
-app.use(express.static(__dirname + '/public'))
-   .use(cors())
-   .use(cookieParser());
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
@@ -18,7 +15,7 @@ const spotifyApi = new SpotifyWebApi({
 
 });
 
-spotifyApi.setAccessToken(process.env.ACCESS);
+// spotifyApi.setAccessToken(process.env.ACCESS);
 
 // spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', {limit:10, offset:20}).then(
 //     function(data) {
@@ -29,9 +26,31 @@ spotifyApi.setAccessToken(process.env.ACCESS);
 //     }
 // );
 
-spotifyApi.searchPlaylists('workout')
-  .then(function(data) {
-    console.log('Found playlists are', util.inspect(data.body.playlists.items[0].id, false, null, true));
-  }, function(err) {
-    console.log('Something went wrong!', err);
+spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+      console.log('The access token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+  
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    function(err) {
+      console.log('Something went wrong when retrieving an access token', err);
+    }
+);
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/post-test', (request, response) => {
+    spotifyApi.searchPlaylists(request.body.theme)
+    .then(function(data) {
+        let playList = util.inspect(data.body.playlists.items[0].id, false, null, true);
+        console.log('Found playlists are', util.inspect(data.body.playlists.items[0].id, false, null, true));
+    }, function(err) {
+        console.log('Something went wrong!', err);
+    });
+    response.sendStatus(200);
 });
+
+
+app.listen(8888, () => console.log("Started on port 8888"));
